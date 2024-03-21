980d1abd5de662c38e55c525ee663383565faf27
Sicilianii <joker92006@mail.ru>
2024-03-21 16:11:49 +0300

    1

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -12,7 +12,9 @@ const handleError = (res, err) => { res.status(500).json( {error: `${err}`} )}
 
 const getAllUsers = (req, res) => {
     User.findById(req.params.id, { friends: 1}).then( currentUser => {
-        User.find({ '_id': { $in: currentUser.friends } }, { nameUser: 1}).then( (friends) => {
+        User.find({ '_id': { $in: currentUser.friends } }, { nameUser: 1, email: 1, 
+            job_title: 1, 
+            phone: 1}).then( (friends) => {
             res.status(200).json(friends);
         } ).catch( (err) => handleError(res, err) )
     })
