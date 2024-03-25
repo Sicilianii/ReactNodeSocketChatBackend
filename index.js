10bf74835967e5ddf25934951c6390ad2eaddc6b
Sicilianii <joker92006@mail.ru>
2024-03-25 21:13:46 +0300

    2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import express from "express";
-import { createServer } from 'node:http';
+import express from 'express';
+import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from 'url';
 import mongoose from 'mongoose';
 import { router as userRouter } from './routes/user-routes.js';
 import { router as mainRouter } from './routes/main-routes.js';
@@ -22,10 +24,6 @@ app.use(function (req, res, next) {
     // next();
 });
 
-
-const server = createServer(app);
-
-
 const URL = 'mongodb://admin:GIH%269zBS@lipascadmeb.beget.app/';
 mongoose
     .connect(URL)
@@ -33,3 +31,19 @@ mongoose
     .catch( (err) => { console.log(`DB connection error ${err}`) });
 
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const expressServer = app.listen(PORT, () => {
+    console.log(`Listening port on : ${PORT}`);
+});
+
+const io = new Server(expressServer, {
+    cors: {
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:3001","http://localhost:63342"]
+    }
+});
