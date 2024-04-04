1045df51991e1fdac4ddd6ae0a401d228b90bc5e
Sicilianii <joker92006@mail.ru>
2024-04-04 17:12:53 +0300

    that's all

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAllUsers, singIn, singUp } from '../controllers/user-controllers.js';
+import { getAllUsers, singIn, singUp, getProfileInfoByID } from '../controllers/user-controllers.js';
 
 export const router = express.Router();
 
 router.get('/user/:id', getAllUsers );
 router.post('/singIn', singIn );
-router.post('/singUp', singUp)
+router.post('/singUp', singUp);
+router.get('/profile/:id', getProfileInfoByID);
