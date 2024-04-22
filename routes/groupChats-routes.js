b28f9056f14eb3aba42b52e5be650b5fb526b207
Vladislav <98421574+Sicilianii@users.noreply.github.com>
2024-04-22 19:29:14 +0300

    Delete routes directory

--- routes/groupChats-routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllGroupChats, getInfoGroupChat, createNewChat } from '../controllers/groupChat-controllers.js';
-
-export const router = express.Router();
-
-router.get('/groups/:id', getAllGroupChats)  // получаем все чаты пользователя
-router.get('/groups/info/:id', getInfoGroupChat); // получаем всю инфу чата
-router.post('/group/new', createNewChat);
--- routes/recentChat-routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {getAllRecentChat, getInfoRecentChat} from "../controllers/recentChat-controllers.js";
-
-export const router = express.Router();
-
-router.get('/recents/:id', getAllRecentChat); // получаем все чаты пользователя
-router.get('/recents/info/:id', getInfoRecentChat); // получаем всю инфу чата
-
-
