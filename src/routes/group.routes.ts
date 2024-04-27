ef41a1e54b6d067152d2539376f28e93d62b8c29
Sicilianii <joker92006@mail.ru>
2024-04-27 14:04:06 +0300

    done fix

--- src/routes/group.routes.ts
+++ src/routes/group.routes.ts
@@ -15,6 +15,7 @@ class GroupRoutes {
         this.router.get('/groups/:id', getAllGroupChats)  // получаем все чаты пользователя
         this.router.get('/groups/info/:id', getInfoGroupChat); // получаем всю инфу чата
         this.router.post('/group/new', createNewChat);
+
         this.router.get('/chats/group/:id', getMessagesGroupChat);
 
     }
